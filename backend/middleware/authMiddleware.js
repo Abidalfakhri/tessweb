644fdee90277645657ff9a202a24a2572774ce79
Rendymanurung <rendyjanuarta32@gmail.com>
2025-11-21 22:46:50 +0700

    Add auth module logic

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,1 +1,10 @@
-// tes
+module.exports = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Token tidak ditemukan" });
+  }
+
+  next();
+};
+
--- backend/services/authService.js
+++ backend/services/authService.js
@@ -1,1 +1,10 @@
-// tes
+module.exports = {
+  async register({ username, password }) {
+    return { username, password, message: "User terdaftar (dummy)" };
+  },
+
+  async login({ _username, _password }) {
+    return { token: "dummy-token" };
+  }
+};
+
